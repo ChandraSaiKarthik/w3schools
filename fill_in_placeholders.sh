67b13bb2717e79998bbb86c3fba068ed9b5197dd
ChandraSaiKarthik <chandrasaikarthik@gmail.com>
2024-07-20 10:21:51 +0530

    dynamic value set

--- fill_in_placeholders.sh
+++ fill_in_placeholders.sh
@@ -10,12 +10,14 @@ log_file_dir_placeholder="<<LogFileDir>>"
 buffer_file_dir_placeholder="<<BufferFileDir>>"
 
 # Define variables to replace placeholders in files
+# var_database_name=${1:-'w3schools'}
 var_database_name='w3schools'
 var_log_file_dir='C:\Program Files (x86)\SQLserver\LogFiles\'
 var_buffer_file_dir='C:\Program Files (x86)\SQLserver\BufferFiles\'
 
 # Replace placeholders with actual values in files in the source directory
-find "$source_dir" -type f -name "*.sql" -exec sed -i "s|$database_placeholder|$var_database_name|g;" {} \;
+#find "$source_dir" -type f -name "*.sql" -exec sed -i "s|$database_placeholder|$var_database_name|g;" {} \;
+find "$source_dir" -type f \( -name "*.sql" -o -name "*.txt" \) -exec sed -i "s|$database_placeholder|$var_database_name|g;" {} \;
 #find "$source_dir" -type f -name "*.sql" -exec sed -i "s|$log_file_dir_placeholder|$var_log_file_dir|g;" {} \;
 #find "$source_dir" -type f -name "*.sql" -exec sed -i "s|$buffer_file_dir_placeholder|$var_buffer_file_dir|g;" {} \;
 
