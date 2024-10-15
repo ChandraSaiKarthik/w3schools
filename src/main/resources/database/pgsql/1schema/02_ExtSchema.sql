75da9c318de9a8cbb2e1a6c52ef4f2d876706f87
ChandraSaiKarthik <chandrasaikarthik@gmail.com>
2024-10-16 00:02:11 +0530

    Initial Commit

---
+++ src/main/resources/database/pgsql/1schema/02_ExtSchema.sql
@@ -0,0 +1,21 @@
+ALTER TABLE public."Categories"
+ALTER COLUMN "Description" SET DATA TYPE text;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'category_type') THEN
+        CREATE TYPE category_type AS ENUM ('Electronics', 'Clothing', 'Groceries', 'Books', 'Furniture');
+    END IF;
+END;
+$$;
+
+ALTER TABLE public."Categories"
+ADD COLUMN if not exists parent_category category_type;
+
+-- timestamp without time zone = timestamp
+ALTER TABLE public."Categories"
+ADD COLUMN if not exists created_on timestamp;
+
+-- timestamp with time zone = timestamptz
+ALTER TABLE public."Categories"
+ADD COLUMN if not exists modified_on timestamptz;
